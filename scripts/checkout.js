35fa976202ca0ec1ae30b87c182dd968309f77a6
Md Amiruddin <md.amiruddin13@gmail.com>
2023-11-01 23:13:20 +0530

    delete products from HTML when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -18,7 +18,7 @@ function renderOrderSummary() {
         });
     
         cartSummaryHTML += `
-            <div class="cart-item-container">
+            <div class="cart-item-container js-cart-item-container-${checkoutProduct.id}">
                 <div class="delivery-date">
                 Delivery date: Tuesday, June 21
                 </div>
@@ -105,5 +105,6 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
     link.addEventListener('click', () => {
         const removeProductId = link.dataset.productId;
         removeFromCart(removeProductId);
+        document.querySelector(`.js-cart-item-container-${removeProductId}`).remove();
     })
 });
